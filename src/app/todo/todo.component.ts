32e8f374be63c67af114e341e76e639ae4d575e0
AbrahamBen <eternelle.vision@outlook.fr>
2022-05-17 03:24:45 +0200

    Attributes and structurals dirctives

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -11,37 +11,47 @@ export class TodoComponent implements OnInit {
   public todoTwo = 'Projet 2'
   public todoThree = 'Projet 3'
   public todoFour = 'Projet 4'
+  public colorText:string = 'blue'
 
   public todos = [
     {
       todoName: 'Projet 1',
       todoStatus: true,
-      image: 'https://via.placeholder.com/150'
+      image: 'https://via.placeholder.com/150',
+      isModif:false
     },
     {
       todoName: 'Projet 2',
       todoStatus : false,
-      image: 'https://via.placeholder.com/150'
+      image: 'https://via.placeholder.com/150',
+      isModif:false
     },
     {
       todoName: 'Projet 3',
       todoStatus : true,
-      image: 'https://via.placeholder.com/150'
+      image: 'https://via.placeholder.com/150',
+      isModif:false
     },
     {
       todoName: 'Projet 4',
       todoStatus : false,
-      image: 'https://via.placeholder.com/150'
+      image: 'https://via.placeholder.com/150',
+      isModif:false
     },
   ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
   public onChangeStatus(index:number){
     this.todos[index].todoStatus = !this.todos[index].todoStatus;
   }
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public onChangeModif(i:number){
+    this.todos[i].isModif = !this.todos[i].isModif;
   }
 
 }
